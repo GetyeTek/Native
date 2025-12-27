3f8252d9f87aa28c6e5339c4f574766a9f0b5324
GetyeTek <getyeteklu2@gmail.com>
2025-12-27 01:06:22 -0800

    Conduit: 1 ops

--- app/src/main/java/com/example/flashlight/FlashlightLogic.kt
+++ app/src/main/java/com/example/flashlight/FlashlightLogic.kt
@@ -1,11 +1,1 @@
-package com.example.flashlight
 
-class FlashlightLogic {
-    var isFlashlightOn: Boolean = false
-        private set
-
-    fun toggle(): Boolean {
-        isFlashlightOn = !isFlashlightOn
-        return isFlashlightOn
-    }
-}
